cd137276287b4d2f2664a6baa8449ce86d38a426
DoctorStone08 <arunavgawri@gmail.com>
2025-03-01 03:04:31 +0530

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
-import Page4 from './Page4';
-import Page5 from './Page5';
-import Page6 from './Page6';
-import Page7 from './Page7';
-import Page8 from './Page8';
-import Final from './Final';
-import {Route,Routes} from "react-router-dom"
-import Home from './Home';
-function App() {
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
+// Dynamically import each page component using React.lazy
+const Home = React.lazy(() => import('./Home'));
+const Page1 = React.lazy(() => import('./Page1'));
+const Page2 = React.lazy(() => import('./Page2'));
+const Page3 = React.lazy(() => import('./Page3'));
+const Page4 = React.lazy(() => import('./Page4'));
+const Page5 = React.lazy(() => import('./Page5'));
+const Page6 = React.lazy(() => import('./Page6'));
+const Page7 = React.lazy(() => import('./Page7'));
+const Page8 = React.lazy(() => import('./Page8'));
+const Final = React.lazy(() => import('./Final'));
+
+function App() {
   return (
-    <>
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/page1" element={<Page1/>}/>
-    <Route path="/page2" element={<Page2/>}/>
-    <Route path="/page3" element={<Page3/>}/>
-    <Route path="/page4" element={<Page4/>}/>
-    <Route path="/page5" element={<Page5/>}/>
-    <Route path="/page6" element={<Page6/>}/>
-    <Route path="/page7" element={<Page7/>}/>
-    <Route path="/page8" element={<Page8/>}/>
-    <Route path="/final" element={<Final/>}/>
-    </Routes>
-         
-    </>
-  )
+    <Suspense fallback={<div>Loading...</div>}> {/* Fallback content while the components are loading */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+        <Route path="/page5" element={<Page5 />} />
+        <Route path="/page6" element={<Page6 />} />
+        <Route path="/page7" element={<Page7 />} />
+        <Route path="/page8" element={<Page8 />} />
+        <Route path="/final" element={<Final />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
